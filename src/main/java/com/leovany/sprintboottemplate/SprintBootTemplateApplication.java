9851dca5d533cd3a25699dfcc517beb45666dbe8
Leovany <348083098@qq.com>
2024-06-08 16:07:21 +0800

    init

---
+++ src/main/java/com/leovany/sprintboottemplate/SprintBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.levany.sprintboottemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/levany/sprintboottemplate/SprintBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levany.sprintboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
